96d295dbac8a207d029306d284dcb49ae5eed95d
tumit <tumit@live.com>
2019-06-17 22:13:53 +0900

    add many config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   resources :templates
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'templates#index'
